bbac8f9228625aa7c04b757b10650089a440b343
David Sawatzky <david@diatonic.io>
2023-01-27 18:54:40 -0600

    fix linting errors

--- types/components/Hero/Hero.d.ts
+++ types/components/Hero/Hero.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="react" />
 import { HeroProps } from './Hero.types';
-declare function Hero({ id, image, imageSize, imagePosition, textBlockPosition, headingText, subText, heroHeight, buttonText, primary, error, disabled, onClick, ...props }: HeroProps): JSX.Element;
+declare function Hero({ id, image, imageSize, imagePosition, textBlockPosition, headingText, subText, heroHeight, error, disabled, ...props }: HeroProps): JSX.Element;
 export default Hero;
--- types/components/Hero/Hero.types.d.ts
+++ types/components/Hero/Hero.types.d.ts
@@ -1,18 +1,15 @@
-import { MouseEventHandler, ReactNode } from 'react';
+import { ReactNode } from 'react';
 export interface HeroProps {
     id?: string;
     error?: boolean;
     disabled?: boolean;
-    primary?: boolean;
     image?: string;
     video?: string;
     headingText?: string;
     subText?: string;
-    buttonText?: string;
     heroHeight?: string;
     imageSize?: 'cover' | 'auto' | 'contain' | string;
     imagePosition?: 'left' | 'center' | 'right';
     textBlockPosition?: 'left' | 'right';
-    onClick?: MouseEventHandler<HTMLButtonElement>;
     children?: ReactNode;
 }
